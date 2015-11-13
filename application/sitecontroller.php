a4f933fb7b07e6d700bb029747a77e1c6572a4aa
Luiz Kowalski <lhkowalski@gmail.com>
2015-11-13 17:46:10 -0200

    alterar controller de teste para testar a classe de imagem

--- application/sitecontroller.php
+++ application/sitecontroller.php
@@ -3,9 +3,14 @@ class SiteController extends Controller
 {
    public function index()
    {
-      $cliente = Cliente::load('id = ?', 1);
-      print_r($cliente);
-      echo "<hr>";
-      print_r($cliente->getSites()[0]->getCliente());
+      $canvas = new Resultado_Imagem('download.jpg');
+      $img1 = new Resultado_Imagem('food.jpg');
+      $img2 = new Resultado_Imagem('food2.jpg');
+
+      $canvas->addImagem($img1, 100, 100);
+      $canvas->addImagem($img2, 440, 0);
+      $canvas->addImagem($img2, 440, 210);
+
+      $canvas->mostrarResultado();
    }
 }
