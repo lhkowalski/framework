65e4c616d443216c35aef2baafdd3ba7b08cd23f
Luiz Kowalski <lhkowalski@gmail.com>
2015-11-19 13:31:10 -0200

    adicionar classe que modela uma requisição

---
+++ core/request.php
@@ -0,0 +1,40 @@
+<?php
+
+class Request
+{
+   protected $_method, $_content;
+
+   public function __construct()
+   {
+      $this->_content = null;
+      $this->_method = $_SERVER['REQUEST_METHOD'];
+   }
+
+   /**
+    * Retorna o conteúdo da requisição. Útil para quando alguma entidade for enviada via PUT ou POST.
+    */
+   public function content()
+   {
+      if($this->_content === null)
+      {
+         $this->_content = file_get_contents("php://input");
+      }
+      
+      return $this->_content;
+   }
+
+   public function method()
+   {  
+      return $this->_method;
+   }
+
+   public function isPost()
+   {
+      return $this->_method === 'POST';
+   }
+
+   public function isGet()
+   {
+      return $this->_method === 'GET';
+   }
+}
