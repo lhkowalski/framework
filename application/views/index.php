f142ac62a10c86c7ca82abbc840c5201997af804
Luiz Kowalski <lhkowalski@gmail.com>
2015-11-19 10:33:29 -0200

    adicionar aplicação de exemplo

---
+++ application/views/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <meta name="description" content="a descrição do seu site em no máximo 90 caracteres">
+        <meta name="keywords" content="escreva palavras-chaves curtas, máximo 150 caracteres">
+        <title>Hello World!</title>
+    </head>
+    <body>
+        <h1>Hello World!</h1>
+    </body>
+</html>
