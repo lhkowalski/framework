c27d4c6172cde32243b4a68433c2ade0c00d0b79
Luiz Kowalski <lhkowalski@gmail.com>
2015-12-26 01:33:23 -0200

    alterar o comportamento da classe de paginação

--- core/pagination.php
+++ core/pagination.php
@@ -1,38 +1,17 @@
-<?php
-
-class Pagination
-{
-   public static function build($base, $total, $atual = 1)
-   {
-      $itens = array();
-      
-      for($i = 1; $i <= $total; $i++)
-      {
-         $class = "link-pagina";
-         if($i == $atual)
-         {
-            $class .= " link-pagina-atual";
-         }
-        // $itens[] = "<a class='$class' href='{$base}{$i}'>$i</a>";
-      }
-
-      if($atual == 1)
-      {
-         $anterior = "<span class='link-pagina'>&laquo; Anterior</span> ";
-         if($total > 1)
-            $proxima = " <a class='link-pagina' href='{$base}2'>Próxima &raquo;</a>";
-         else
-            $proxima = " <span class='link-pagina'>Próxima &raquo;</span>";
-      }
-      else
-      {
-         $anterior = "<a class='link-pagina' href='$base".($atual-1)."'>&laquo; Anterior</a> ";
-         if($atual >= $total)
-            $proxima = " <span class='link-pagina'>Próxima &raquo;</span>";
-         else
-            $proxima = " <a class='link-pagina' href='{$base}".($atual+1)."'>Próxima &raquo;</a>";
-      }
-      
-      return $anterior.implode(" ", $itens).$proxima;
-   }
+<?php
+
+class Pagination
+{
+   public static function build($base, $total, $atual = 1)
+   {
+      $template = new Template('views/pagination.php');
+
+      if($total > 15)
+         $total = 15;
+
+      $template->base = $base;
+      $template->total = $total;
+      $template->atual = $atual;
+      return $template;
+   }
 }
