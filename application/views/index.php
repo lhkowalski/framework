6abfb8f1a3bd824e1987bc7e805796eb587d6f2c
Luiz Kowalski <lhkowalski@gmail.com>
2017-12-22 17:20:52 -0300

    citar no index de exemplo a existência do bootstrap

--- application/views/index.php
+++ application/views/index.php
@@ -4,6 +4,11 @@
         <meta charset="utf-8" />
         <meta name="description" content="a descrição do seu site em no máximo 90 caracteres">
         <meta name="keywords" content="escreva palavras-chaves curtas, máximo 150 caracteres">
+        <!-- Última versão CSS compilada e minificada -->
+			<link rel="stylesheet" href="<?php echo ROOT_URL; ?>/css/bootstrap.min.css">
+
+			<!-- Última versão JavaScript compilada e minificada -->
+			<script src="<?php echo ROOT_URL; ?>/js/bootstrap.min.js"></script>
         <title>Hello World!</title>
     </head>
     <body>
