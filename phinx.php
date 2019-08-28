359bd2f6a296519bb495ca333169cc0f6c0f4f48
Luiz Kowalski <lhkowalski@gmail.com>
2019-08-28 18:02:10 -0300

    corrigir nome da variável de HOST; inicializar as variáveis com load() antes de usar

--- phinx.php
+++ phinx.php
@@ -3,6 +3,9 @@
 // change this, if you changes the core directory
 require './core/config.php';
 
+// load settings
+Config::load();
+
 return [
     "paths" => [
         "migrations" => "application/migrations"
@@ -12,7 +15,7 @@ return [
         "default_database" => "default",
         "default" => [
             "adapter" => Config::get('DB_DRIVER'),
-            "host" => Config::get('DB_DRIVER'),
+            "host" => Config::get('DB_HOST'),
             "name" => Config::get('DB_NAME'),
             "user" => Config::get('DB_USER'), 
             "pass" => Config::get('DB_PASS'), 
