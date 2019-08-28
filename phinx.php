9f98bd90bc6a825674cc9ad09c42f6de5560e285
Luiz Kowalski <lhkowalski@gmail.com>
2019-08-28 16:17:46 -0300

    incluir configuração do Phinx, importando das configurações globais

---
+++ phinx.php
@@ -0,0 +1,22 @@
+<?php
+
+// change this, if you changes the core directory
+require './core/config.php';
+
+return [
+    "paths" => [
+        "migrations" => "application/migrations"
+    ],
+    "environments" => [
+        "default_migration_table" => "phinxlog",
+        "default_database" => "default",
+        "default" => [
+            "adapter" => Config::get('DB_DRIVER'),
+            "host" => Config::get('DB_DRIVER'),
+            "name" => Config::get('DB_NAME'),
+            "user" => Config::get('DB_USER'), 
+            "pass" => Config::get('DB_PASS'), 
+            "port" => Config::get('DB_PORT'), 
+        ]
+    ]
+];
